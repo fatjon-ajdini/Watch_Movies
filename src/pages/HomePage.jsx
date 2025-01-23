3bdad694a4e6a847008a91faccd8ac23444d4a52
Fatjon Ajdini <fatjonajdini11@gmail.com>
2025-01-23 10:56:47 +0100

    HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import MovieListings from "../components/MovieListings";
+import ViewAllMovies from "../components/ViewAllMovies";
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <Navbar />
       <Hero />
       <MovieListings />
+      <ViewAllMovies />
     </>
   );
 };
