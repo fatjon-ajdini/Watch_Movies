169fd0197b7e8a3054b0a8a75d5bcad013b566ae
Fatjon Ajdini <fatjonajdini11@gmail.com>
2025-01-23 09:14:02 +0100

    Limit movies to 6

--- src/components/MovieListings.jsx
+++ src/components/MovieListings.jsx
@@ -3,11 +3,13 @@ import movies from "../movies.json";
 import MovieListing from "./MovieListing";
 
 const MovieListings = () => {
+  const recentMovies = movies.slice(0, 6);
+
   console.log(movies);
   return (
     <>
       <div className="container mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
-        {movies.map((movie) => (
+        {recentMovies.map((movie) => (
           <MovieListing key={movie.id} movie={movie} />
         ))}
       </div>
