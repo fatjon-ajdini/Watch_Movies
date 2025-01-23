62b69a67f78ba1fa562253c7b98d26e000b51d63
Fatjon Ajdini <fatjonajdini11@gmail.com>
2025-01-23 15:53:17 +0100

    Setup server and fetching

--- src/components/MovieListings.jsx
+++ src/components/MovieListings.jsx
@@ -1,15 +1,31 @@
 import React from "react";
-import movies from "../movies.json";
+import { useState, useEffect } from "react";
 import MovieListing from "./MovieListing";
 
 const MovieListings = ({ isHome = false }) => {
-  const movieListings = isHome ? movies.slice(0, 6) : movies;
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const res = await fetch("http://localhost:5050/movies");
+        const data = await res.json();
+        setMovies(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchMovies();
+  }, []);
 
   console.log(movies);
   return (
     <>
       <div className="container mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
-        {movieListings.map((movie) => (
+        {movies.map((movie) => (
           <MovieListing key={movie.id} movie={movie} />
         ))}
       </div>
