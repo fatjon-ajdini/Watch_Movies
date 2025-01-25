65d4a629d3cc60fa9ea01f3fe4ede387544dd5aa
Fatjon Ajdini <fatjonajdini11@gmail.com>
2025-01-25 14:23:19 +0100

    Sort

--- src/components/MovieListings.jsx
+++ src/components/MovieListings.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { useState, useEffect } from "react";
 import MovieListing from "./MovieListing";
 import Spinner from "./Spinner";
+import Sort from "./Sort";
 
 const MovieListings = ({ isHome = false }) => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [sort, setSort] = useState(isHome);
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -28,10 +30,13 @@ const MovieListings = ({ isHome = false }) => {
       {loading ? (
         <Spinner loading={loading} />
       ) : (
-        <div className="relative container mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
-          {movies.map((movie) => (
-            <MovieListing key={movie.id} movie={movie} />
-          ))}
+        <div className="grid grid-row mt-5">
+          {sort ? "" : <Sort />}
+          <div className="relative container mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
+            {movies.map((movie) => (
+              <MovieListing key={movie.id} movie={movie} />
+            ))}
+          </div>
         </div>
       )}
     </>
---
+++ src/components/Sort.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { Link } from "react-router-dom";
+
+const Sort = () => {
+  return (
+    <Menu as="div" className="w-100 mx-auto relative inline-block text-left">
+      <div>
+        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50">
+          SORT
+          <ChevronDownIcon
+            aria-hidden="true"
+            className="-mr-1 size-5 text-gray-400"
+          />
+        </MenuButton>
+      </div>
+
+      <MenuItems
+        transition
+        className="absolute right-0 z-10 mt-2 w-20 origin-top-right rounded-md bg-white ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+      >
+        <div className="py-1">
+          <MenuItem>
+            <Link
+              to="/movies/#"
+              className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+            >
+              Year
+            </Link>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </Menu>
+  );
+};
+
+export default Sort;
