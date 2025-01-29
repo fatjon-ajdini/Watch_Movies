288bd545ee57b6671d0ace670ef0e7ff6694c56d
Fatjon Ajdini <fatjonajdini11@gmail.com>
2025-01-29 12:32:01 +0100

    SearchBar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { FaSearch } from "react-icons/fa";
+
 import { useState } from "react";
 import SearchResult from "./SearchResult";
+
+import { FaSearch } from "react-icons/fa";
+
+import CloseIcon from "@mui/icons-material/Close";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Searchbar = () => {
   const [input, setInput] = useState("");
@@ -28,44 +33,40 @@ const Searchbar = () => {
     fetchMovies(value);
   };
 
+  const handleClose = () => {
+    setInput("");
+    setResults([]);
+  };
+
   return (
     <>
-      <div className="max-w-md mx-auto">
-        <label
-          htmlFor="default-search"
-          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-        >
-          Search
-        </label>
-        <div className="relative mt-8">
-          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <FaSearch id="search-icon" className="text-sky-600" />
-          </div>
+      <section className="max-w-sm mx-auto">
+        <div className="relative mt-8 flex flex-column">
           <input
-            type="search"
+            type="text"
             id="default-search"
             className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Search for Movies"
             value={input}
             onChange={(e) => handleChange(e.target.value)}
-            required
           />
-          <button
-            type="submit"
-            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-sky-700 dark:focus:blue-800"
-          >
-            Search
-          </button>
+          <div className="absolute inset-y-0 right-0 mr-2">
+            <div className="pt-2">
+              {input === "" ? (
+                <SearchIcon fontSize="large" />
+              ) : (
+                <CloseIcon fontSize="large" onClick={handleClose} />
+              )}
+            </div>
+          </div>
         </div>
-        {/* <div className="mt-3">
-          <SearchResultListing results={results} />
-        </div> */}
+
         <div className="mt-3 max-h-40 overflow-auto rounded-lg bg-sky-900 shadow-sm z-30">
           {results.map((result, id) => {
             return <SearchResult result={result} key={id} />;
           })}
         </div>
-      </div>
+      </section>
     </>
   );
 };
