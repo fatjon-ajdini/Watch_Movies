278dd9e8eeb8704c44a4f2f93d4fa552d5146251
Fatjon Ajdini <fatjonajdini11@gmail.com>
2025-02-17 21:00:30 +0100

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Watch_Movies",
   server: {
     port: 3000,
     proxy: {
